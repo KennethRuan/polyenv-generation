198ef54f25846d6129065c3fba80569843b93388
KennethRuan <kennethjruan@gmail.com>
2022-01-10 16:10:04 -0500

    added arclength calculation and interface features

--- src/calc.py
+++ src/calc.py
@@ -1,6 +1,9 @@
 import bisect
 import numpy as np
+import sympy as sy
+import math
 
+polynomialfit = np.polynomial.polynomial.Polynomial.fit
 """
 Polyfit(x, y, l, r)
 x - A list of x-coords
@@ -16,10 +19,11 @@ def polyfit(x, y, l, r):
     old_fit = -1
     cur_fit = 0
     n=1
-    while old_fit < cur_fit and n <= 20:
+    while old_fit < cur_fit and n <= min(len(x)-1,20):
         old_fit = cur_fit
 
-        coeffs = np.polyfit(x, y, n)
+        data = polynomialfit(x, y, n, [l,r])
+        coeffs = data.convert().coef
 
         # Find r^2 to determine fit
         p = np.poly1d(coeffs)
@@ -30,12 +34,13 @@ def polyfit(x, y, l, r):
         cur_fit = ssreg / sstot
         n += 1
 
-    coeffs = np.polyfit(x, y, n-1)
-    results['polynomial'] = coeffs.tolist()
+    data = polynomialfit(x, y, n-1, [l,r])
+    coeffs = data.convert().coef
+    results['polynomial'] = coeffs
     results['determination'] = old_fit
 
     print(n-1)
-    poly = np.poly1d(coeffs)
+    poly = np.poly1d(coeffs[::-1])
     nx = [_ for _ in range(l, r+1)]
     ny = poly(nx)
 
@@ -45,3 +50,25 @@ def polyfit(x, y, l, r):
         points.append((nx[i], ny[i]))
     results['points'] = points
     return results
+
+def arclength(l, r, coeffs):
+    # Taking the derivative of the polynomial
+    deriv = [coeffs[i]*i for i in range(1, len(coeffs))] 
+    poly = np.poly1d(deriv[::-1])
+
+    # print(deriv)
+    # Arc length function
+    def f(x):
+        nx = x
+        for i in range(len(nx)):
+            # print(1+poly(nx[i])**2)
+            nx[i] = math.sqrt(1+poly(nx[i])**2)
+        return nx
+
+    # Riemann Sum to approximate integration on the arc length function
+    N = 10000
+    dx = (r-l)/N
+    x_midpoint = np.linspace(dx/2, r - dx/2, N)
+    riemann_sum = np.sum(f(x_midpoint) * dx)
+
+    return riemann_sum 
--- src/window.py
+++ src/window.py
@@ -5,6 +5,7 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 midnight_blue = pygame.Color("#23283D")
 dark_navy = pygame.Color("#101116")
+mid_blue = pygame.Color("#323F5C")
 
 # Define Screen Sizes
 SCREEN_W = 960
@@ -23,6 +24,7 @@ mouse_down = False
 update_screen = False
 lobf_exists = False
 button_clicked = -1
+slider_clicked = -1
 point_list = []
 lobf = {}
 
@@ -44,7 +46,7 @@ class Button():
         if self.hasImage:
             screen.blit(self.image, (self.rect.x, self.rect.y))
         else:
-            pygame.draw.rect(screen, dark_navy, self.rect)
+            pygame.draw.rect(screen, dark_navy, self.rect, border_radius=10)
             txt = self.font.render(self.text, True, white)
             txt_rect = txt.get_rect(center=self.rect.center)
             screen.blit(txt, txt_rect)
@@ -52,12 +54,52 @@ class Button():
     def clicked(self):
         self.func()
 
+class Slider():
+    def __init__(self, val, mini, maxi, x, y, w=920, h=50):
+        self.val = val
+        self.mini = mini
+        self.maxi = maxi
+        self.per = (val-mini)/maxi
+        
+        # bar dimensions
+        self.b_x = x
+        self.b_y = y
+        self.b_w = w
+        self.b_h = h
+        self.b_rect = pygame.Rect(self.b_x, self.b_y, self.b_w, self.b_h)
+
+        # slider dimensions
+        self.s_x = self.b_x+self.per*(self.b_w)
+        self.s_y = y
+        self.s_w = 10
+        self.s_h = 30
+        self.s_rect = pygame.Rect(0, 0, self.s_w, self.s_h)
+        self.s_rect.center = (self.s_x, self.b_y+self.b_h//2)
+
+    def draw(self):
+        pygame.draw.rect(screen, dark_navy, self.b_rect, border_radius=10)
+        pygame.draw.rect(screen, mid_blue, self.s_rect)
+
+    def setVal(self, val):
+        self.val = max(self.mini, min(self.b_w, val))
+        self.per = (self.val-self.mini)/(self.b_w)
+        self.s_rect.centerx = self.b_x+(self.per*self.b_w)
+        # print("val", self.val, "per", self.per, "x", self.s_rect.centerx)
 
 # Initialize UI components
 buttons = []
+sliders = []
 # Button 1
 def_button = Button(20, VIEWPORT_H+20, text="FIT")
 buttons.append(def_button)
+
+# Slider 1
+left_slider = Slider(100, 0, VIEWPORT_W-20, 20, 450, VIEWPORT_W-40, 15)
+sliders.append(left_slider)
+
+#Slider 2
+right_slider = Slider(800, 0, VIEWPORT_W-20, 20, 500, VIEWPORT_W-40, 15)
+sliders.append(right_slider)
 
 def pixel(surface, color, pos):
     # pygame.draw.line(surface, color, pos, pos)
@@ -66,6 +108,8 @@ def pixel(surface, color, pos):
 def drawUI():
     pygame.draw.rect(screen, midnight_blue, (0,VIEWPORT_H,VIEWPORT_R,SCREEN_H))
     def_button.draw()
+    left_slider.draw()
+    right_slider.draw()
     
 screen.fill(white)
 drawUI()
@@ -85,35 +129,49 @@ while run:
                 point_list.clear()
                 update_screen = True
             if event.key == pygame.K_c:
-                x_vals, y_vals = zip(*point_list)
-                # print(x_vals)
-                # print(y_vals)
-                lobf = polyfit(x_vals, y_vals, 0, VIEWPORT_W)
-                lobf_exists = True
-                # print(lobf["points"])
-                update_screen = True
+                if point_list:
+                    x_vals, y_vals = zip(*point_list)
+                    # print(x_vals)
+                    # print(y_vals)
+                    lobf = polyfit(x_vals, y_vals, 0, VIEWPORT_W)
+                    lobf_exists = True
+                    # print(lobf["points"])
+                    update_screen = True
+            if event.key == pygame.K_r:
+                if lobf_exists:
+                    print(arclength(left_slider.s_rect.centerx, right_slider.s_rect.centerx, lobf["polynomial"]))
 
         if mouse_down:
             x, y = pygame.mouse.get_pos()
-            if VIEWPORT_L <= x <= VIEWPORT_R and y <= VIEWPORT_H: 
-                bisect.insort(point_list, (x, y))
-                update_screen = True
-            for i, button in enumerate(buttons):
-                if button.rect.collidepoint((x,y)):
-                    button_clicked = i
+            if button_clicked == -1 and slider_clicked == -1:
+                if VIEWPORT_L <= x <= VIEWPORT_R and y <= VIEWPORT_H: 
+                    bisect.insort(point_list, (x, y))
+                    update_screen = True
+                for i, button in enumerate(buttons):
+                    if button.rect.collidepoint(x, y):
+                        button_clicked = i
+                for i, slider in enumerate(sliders):
+                    if slider.s_rect.collidepoint(x, y):
+                        # print(i, "clicked")
+                        slider_clicked = i
         else: # mouse up
+            # Processing button presses
+            # Only processes upon mouse up
+            if button_clicked == 0:
+                if point_list:
+                    x_vals, y_vals = zip(*point_list)
+                    lobf = polyfit(x_vals, y_vals, 0, VIEWPORT_W)
+                    lobf_exists = True
+                    update_screen = True
             button_clicked = -1
+            slider_clicked = -1
 
-    # Processing button presses
-    if button_clicked == 0:
-        try:
-            x_vals, y_vals = zip(*point_list)
-        except:
-            pass
-        lobf = polyfit(x_vals, y_vals, 0, VIEWPORT_W)
-        lobf_exists = True
-        update_screen = True
-
+        # Processing slider movement
+        if slider_clicked >= 0:
+            x, y = pygame.mouse.get_pos()
+            sliders[slider_clicked].setVal(x - sliders[slider_clicked].b_x)
+            update_screen = True
+            
     # Processing visual updates
     if update_screen:
         update_screen = False
@@ -126,6 +184,18 @@ while run:
             for i in range(0, len(lobf["points"])-1):
                 pygame.draw.rect(screen, (50,120,50), (func_points[i][0],func_points[i+1][1], func_points[i+1][0]-func_points[i][0], VIEWPORT_H-func_points[i+1][1]))
                 pygame.draw.line(screen, (255, 0, 0), func_points[i], func_points[i+1])
+
+            # Red Pole controlled by slider 0
+            red_pole = pygame.Rect(0,0,2,30)
+            red_pole.centerx = sliders[0].s_rect.centerx
+            red_pole.bottom = lobf["points"][red_pole.centerx][1]
+            pygame.draw.rect(screen, (255,0,0), (red_pole))
+
+            # Blue Pole controlled by slider 1
+            blue_pole = pygame.Rect(0,0,2,30)
+            blue_pole.centerx = sliders[1].s_rect.centerx
+            blue_pole.bottom = lobf["points"][blue_pole.centerx][1]
+            pygame.draw.rect(screen, (0,0,255), (blue_pole))
 
         for pos in point_list:
             pixel(screen, black, pos)
